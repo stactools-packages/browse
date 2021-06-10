c97b0e4f99e7113b5b780705c05cbd10acaf2304
Pete Gadomski <pete.gadomski@gmail.com>
2021-06-10 08:08:34 -0600

    Convert to standalone repository

--- src/stactools/browse/__init__.py
+++ src/stactools/browse/__init__.py
@@ -4,3 +4,6 @@ def register_plugin(registry):
     from stactools.browse import commands
 
     registry.register_subcommand(commands.browse_command)
+
+
+__version__ = '0.1.6'
---
+++ tests/test_module.py
@@ -0,0 +1,8 @@
+import unittest
+
+import stactools.browse
+
+
+class TestModule(unittest.TestCase):
+    def test_version(self):
+        self.assertIsNotNone(stactools.browse.__version__)
