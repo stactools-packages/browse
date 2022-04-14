88054b37a6b4ec20a4ca8884a804881639e58de2
Pete Gadomski <pete.gadomski@gmail.com>
2022-04-14 09:51:30 -0600

    fix: apply black and typing

--- src/stactools/browse/commands.py
+++ src/stactools/browse/commands.py
@@ -1,57 +1,58 @@
 import os
 import shutil
-from tempfile import TemporaryDirectory
 from subprocess import Popen, call
+from tempfile import TemporaryDirectory
 
 import click
 import jinja2
 from pystac.utils import make_absolute_href
 
 
-def launch_browser(catalog_uri):
+def launch_browser(catalog_uri: str) -> None:
     catalog_uri = make_absolute_href(catalog_uri)
 
     catalog_dir = os.path.dirname(catalog_uri)
     catalog_filename = os.path.basename(catalog_uri)
 
     with TemporaryDirectory() as tmp_dir:
-        for fname in ['Dockerfile-node', 'Dockerfile-titiler']:
+        for fname in ["Dockerfile-node", "Dockerfile-titiler"]:
             p = os.path.join(os.path.dirname(__file__), fname)
             shutil.copy(p, os.path.join(tmp_dir, fname))
 
         # Load template docker-compose
-        template_loader = jinja2.FileSystemLoader(
-            searchpath=os.path.dirname(__file__))
+        template_loader = jinja2.FileSystemLoader(searchpath=os.path.dirname(__file__))
         template_env = jinja2.Environment(loader=template_loader)
         template_file = "docker-compose.yml.template"
         template = template_env.get_template(template_file)
 
         rendered_docker_compose = template.render(
-            catalog_dir=catalog_dir, catalog_filename=catalog_filename)
+            catalog_dir=catalog_dir, catalog_filename=catalog_filename
+        )
 
         compose_file = os.path.join(tmp_dir, "docker-compose.yml")
-        with open(compose_file, 'w') as f:
+        with open(compose_file, "w") as f:
             f.write(rendered_docker_compose)
 
         curdir = os.path.abspath(os.curdir)
         try:
             os.chdir(tmp_dir)
-            p = Popen(['docker-compose', '-f', compose_file, 'up'])
-            p.wait()
+            process = Popen(["docker-compose", "-f", compose_file, "up"])
+            process.wait()
         finally:
-            call(['docker-compose', '-f', compose_file, 'kill'])
-            p.terminate()
+            call(["docker-compose", "-f", compose_file, "kill"])
+            process.terminate()
             os.chdir(curdir)
 
 
-def browse_command(cli):
-
+def browse_command(cli: click.Group) -> click.Command:
     @cli.command(
-        'browse',
-        short_help=('Launch a local stac-browser and tiler via docker '
-                    'to browse a STAC'))
-    @click.argument('catalog')
-    def cmd(catalog):
+        "browse",
+        short_help=(
+            "Launch a local stac-browser and tiler via docker " "to browse a STAC"
+        ),
+    )
+    @click.argument("catalog")
+    def cmd(catalog: str) -> None:
         """Launch docker containers that serve the STAC at CATALOG through stac-browser.
 
         This requires docker to be installed. This will build and run 3 containers,
